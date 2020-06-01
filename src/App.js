064107f4b262e79a020dc6ff59b8eb6259c3c9cf
Martin Petron <martinpetron@outlook.com>
2020-06-01 19:41:45 -0300

    correction to let it work

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ const TopicsList = props => {
   return (
     <div>
       <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+      <Link to={`${props.match.url}13`}>TO TOPIC 13</Link>
+      <Link to={`${props.match.url}17`}>TO TOPIC 17</Link>
+      <Link to={`${props.match.url}21`}>TO TOPIC 21</Link>
     </div>
   );
 };
